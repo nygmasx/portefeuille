ed9bef5cf9283dafce9760a9198a5eda0766ddf5
nygmasx <imraneeslk@gmail.com>
2024-02-21 11:39:58 +0100

    WIP

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -46,27 +46,35 @@ export const Main = () => {
                         </motion.div>
                         <motion.div whileHover={{scale: 1.02}} transition={{ease: "easeInOut"}}
                             className="col-span-2 rounded-[64px] max-w-full h-full bg-[#1D1D1D] flex justify-center items-center">
-                            <div className="w-full max-w-[85%] h-full max-h-[70%] flex flex-col">
+                            <div className="w-full max-w-[85%] h-full max-h-[80%] flex flex-col">
                                 <div className="flex justify-between items-center">
-                                    <h2 className=" flex items-center text-[24px] font-poppins">
+                                    <h2 className=" flex items-center 2xl:text-[24px] text-[22px] font-poppins">
                                         Last use cases on
                                         <FaXTwitter className="ml-1"/></h2>
                                     <motion.a whileHover={{scale: 1.2}} href=""><AiOutlineLink/></motion.a>
                                 </div>
                                 <p>
-                                    Take a look, there is a bunch of projects that you can like
+                                    Sometimes i like to build things on public
                                 </p>
                                 <hr className="w-full h-px mt-4 mb-6 border-0 bg-gray-300"/>
-                                <Avatar>
-                                    <AvatarImage src="https://pbs.twimg.com/profile_images/1638294304719347712/w7Sf9nsH_400x400.jpg" alt="nygmapfp"/>
-                                    <AvatarFallback></AvatarFallback>
-                                </Avatar>
-
+                                <div className="flex w-full">
+                                    <Avatar className="w-[60px] h-[60px]">
+                                        <AvatarImage
+                                            src="https://pbs.twimg.com/profile_images/1638294304719347712/w7Sf9nsH_400x400.jpg"
+                                            alt="nygmapfp"/>
+                                        <AvatarFallback>CN</AvatarFallback>
+                                    </Avatar>
+                                    <div className="mt-1 ml-2">
+                                        <p> notorious nygma 💻</p>
+                                        <p className="text-sm text-emerald-300">@nygmasl</p>
+                                    </div>
+                                </div>
+                                <div className="flex justify-center mt-8">
+                                    <a className="flex items-center px-6 py-1 border rounded" href="">Read My <FaXTwitter className="ml-1"/></a>
+                                </div>
                             </div>
                         </motion.div>
                     </div>
-
-
                 </div>
             </div>
         </main>
